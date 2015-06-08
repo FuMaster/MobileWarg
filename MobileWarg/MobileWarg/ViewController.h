328b0c88bdd9a90ee1eb681711c73e357cff60b7
Lawrence Fu <Lawrence@v1020-wn-162-118.campus-dynamic.uwaterloo.ca>
2015-06-08 18:10:01 -0400

    init

---
+++ MobileWarg/MobileWarg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileWarg
+//
+//  Created by Lawrence Fu on 6/8/15.
+//  Copyright (c) 2015 MobileWarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
