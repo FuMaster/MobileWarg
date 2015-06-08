177ff772bd7c4bea930e267c0f555a9cae9da33b
David Jeong <djeong747@gmail.com>
2015-06-08 18:30:44 -0400

    Added two new views and two view controller files.

---
+++ MobileWarg/MobileWarg/StreamReceiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  StreamReceiveViewController.h
+//  MobileWarg
+//
+//  Created by David Jeong on 2015. 6. 8..
+//  Copyright (c) 2015 MobileWarg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StreamReceiveViewController : UIViewController
+
+@end
