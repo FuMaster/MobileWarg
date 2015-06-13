2661627f3cbf08f300f9f8b70b8c2d34408a8e59
David Jeong <djeong747@gmail.com>
2015-06-13 15:58:30 -0400

    Support for multiple orientations.

--- MobileWarg/MobileWarg/StreamSendViewController.h
+++ MobileWarg/MobileWarg/StreamSendViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface StreamSendViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface StreamSendViewController : UIViewController <UINavigationControllerDelegate>
 
 @end
--- MobileWarg/MobileWarg/StreamSendViewController.m
+++ MobileWarg/MobileWarg/StreamSendViewController.m
@@ -10,7 +10,14 @@
 #import "StreamSendViewController.h"
 
 @interface StreamSendViewController ()
+
 @property (strong, nonatomic) IBOutlet UIView *imageView;
+
+@property (strong, nonatomic) AVCaptureDevice *videoCaptureDevice;
+@property (strong, nonatomic) AVCaptureDeviceInput *videoInput;
+@property (strong, nonatomic) AVCaptureVideoDataOutput *outputData;
+@property (strong, nonatomic) AVCaptureSession *captureSession;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
 
 @end
 
@@ -19,35 +26,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleBottomMargin |
+    UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin |
+    UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
+    
     if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, this application won't work because camera does not exist." delegate:nil cancelButtonTitle:@"Confirm" otherButtonTitles:nil];
         [alert show];
     } else {
         // Create a capture session.
-        AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
-        captureSession.sessionPreset = AVCaptureSessionPresetMedium;
+        self.captureSession = [[AVCaptureSession alloc] init];
+        self.captureSession.sessionPreset = AVCaptureSessionPresetMedium;
         
         // Add capture device.
-        AVCaptureDevice *videoCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-        AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:videoCaptureDevice error:nil];
+        self.videoCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        self.videoInput = [AVCaptureDeviceInput deviceInputWithDevice:self.videoCaptureDevice error:nil];
         
-        if (videoInput) {
+        if (self.videoInput) {
             // If capture device exists.
-            AVCaptureVideoDataOutput *outputData = [[AVCaptureVideoDataOutput alloc] init];
-            outputData.videoSettings = @{(NSString *)kCVPixelBufferPixelFormatTypeKey:@(kCVPixelFormatType_32BGRA)};
+            self.outputData = [[AVCaptureVideoDataOutput alloc] init];
+            self.outputData.videoSettings = @{(NSString *)kCVPixelBufferPixelFormatTypeKey:@(kCVPixelFormatType_32BGRA)};
             
             // Add input and output.
-            [captureSession addInput:videoInput];
-            [captureSession addOutput:outputData];
+            [self.captureSession addInput:self.videoInput];
+            [self.captureSession addOutput:self.outputData];
             
             // Create preview layer.
-            AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:captureSession];
-            previewLayer.frame = self.imageView.bounds;
-            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-            [self.imageView.layer addSublayer:previewLayer];
+            self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.captureSession];
+            self.previewLayer.frame = self.imageView.bounds;
+            self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+            
+            [[NSNotificationCenter defaultCenter] addObserver:self
+                                                     selector:@selector(changedOrientation)
+                                                         name:UIDeviceOrientationDidChangeNotification
+                                                       object:nil];
+            
+            [self.imageView.layer addSublayer:self.previewLayer];
+
             
             // Start running the capture session.
-            [captureSession startRunning];
+            [self.captureSession startRunning];
         } else {
             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Sorry, video input does not exist." delegate:nil cancelButtonTitle:@"Confirm" otherButtonTitles:nil];
             [alert show];
@@ -58,6 +76,29 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) changedOrientation
+{
+    // Change the fit of the UI element.
+    self.previewLayer.frame = self.imageView.bounds;
+    
+    UIDeviceOrientation deviceOrientation = [UIDevice currentDevice].orientation;
+    
+    // Switch statement
+    switch (deviceOrientation) {
+        case UIInterfaceOrientationLandscapeLeft: {
+            [self.previewLayer.connection setVideoOrientation:AVCaptureVideoOrientationLandscapeLeft];
+        } break;
+        case UIInterfaceOrientationLandscapeRight: {
+            [self.previewLayer.connection setVideoOrientation:AVCaptureVideoOrientationLandscapeRight];
+        } break;
+        case UIInterfaceOrientationPortrait:{
+            [self.previewLayer.connection setVideoOrientation:AVCaptureVideoOrientationPortrait];
+        } break;
+        default:
+            break;
+    }
 }
 
 /*
